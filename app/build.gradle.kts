77d52fb5957dde5427a97962389c86f1fa5336f2
JoseLlontop <josellontop100@gmail.com>
2024-09-09 10:30:10 -0300

    Se agrego la dependencia de DataStore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,5 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation(libs.androidx.navigation.compose)
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
 }
